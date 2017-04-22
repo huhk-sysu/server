ff44c69135c942072fa736fbd53d6b706ab86b0d
hhk <codeforsomeone@gmail.com>
2017-04-22 15:46:06 +0800

    精简无用部分

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var path = require('path')
+// var path = require('path')
 // var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
@@ -7,13 +7,8 @@ var bodyParser = require('body-parser')
 const restc = require('restc')
 
 var index = require('./routes/index')
-var users = require('./routes/users')
 var songs = require('./routes/songs')
 var app = express()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,11 +16,11 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use(restc.express())
 
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header('Access-Control-Allow-Credentials', 'true')
@@ -33,9 +28,7 @@ app.all('*', function (req, res, next) {
 })
 
 app.use('/', index)
-app.use('/users', users)
 app.use('/songs', songs)
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -50,9 +43,8 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  console.log(err)
 })
 
 module.exports = app
